d834fa2130bef8d57a22354aa280d22d6f763d64
Sergii Budak <corund1976@gmail.com>
2021-10-27 22:40:44 +0300

    Fin

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -39,10 +39,14 @@ function TransactionHistory({ items }) {
 };
 
 TransactionHistory.propTypes = {
-  id: PropTypes.string,
-  type: PropTypes.string,
-  amount: PropTypes.string,
-  currency: PropTypes.string
-};
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired
+    }).isRequired
+  )
+}
 
 export default TransactionHistory;
